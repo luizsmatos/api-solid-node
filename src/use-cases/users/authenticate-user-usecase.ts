c8acda912e2bf608d471a0e14ea08ecf797abdd1
gusstavopdm@gmail.com <gusstavopdm@gmail.com>
2023-03-18 12:36:53 -0300

    feat: add AuthenticateUserUseCase

---
+++ src/use-cases/users/authenticate-user-usecase.ts
@@ -0,0 +1,38 @@
+import { User } from '@prisma/client'
+import { UsersRepository } from '@/repositories/users/users-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { compare } from 'bcryptjs'
+
+interface AuthenticateUserUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUserUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUserUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUserUseCaseRequest): Promise<AuthenticateUserUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
