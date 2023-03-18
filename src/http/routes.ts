b150b88343272a52cf19e73a897a63c8b112893a
gusstavopdm@gmail.com <gusstavopdm@gmail.com>
2023-03-18 12:44:59 -0300

    feat: add session route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from 'fastify'
 
+import { AuthenticateUserController } from './controllers/users/authenticate-user-controller'
 import { RegisterUserController } from './controllers/users/register-user-controller'
 
 const registerUserController = new RegisterUserController()
+const authenticateUserController = new AuthenticateUserController()
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerUserController.handle)
+  app.post('/session', authenticateUserController.handle)
 }
