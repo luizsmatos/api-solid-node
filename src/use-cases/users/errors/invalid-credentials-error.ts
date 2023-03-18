de48045d6446043614ada1b9bdc97c35cec2ef44
gusstavopdm@gmail.com <gusstavopdm@gmail.com>
2023-03-18 12:50:35 -0300

    fix(InvalidCredentialsError): fix name

--- src/use-cases/users/errors/invalid-credentials-error.ts
+++ src/use-cases/users/errors/invalid-credentials-error.ts
@@ -1,6 +1,6 @@
 export class InvalidCredentialsError extends Error {
   constructor() {
     super('Invalid credentials.')
-    this.name = 'UserNotFound'
+    this.name = 'InvalidCredentials'
   }
 }
