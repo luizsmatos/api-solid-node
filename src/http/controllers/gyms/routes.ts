04f33b66f729088477cb0306b5705c33ccfe39ab
Luiz Gustavo <gusstavopdm@gmail.com>
2023-04-08 13:06:09 -0300

    ✨ feat(routes.ts): add routes for gyms search, nearby list and create gym. Add verifyJWT middleware to all routes.

---
+++ src/http/controllers/gyms/routes.ts
@@ -0,0 +1,15 @@
+import { FastifyInstance } from 'fastify'
+
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { searchGymsController } from './search-gyms-controller'
+import { listNearbyGymsController } from './list-nearby-gyms-controller'
+import { createGymController } from './create-gym-controller'
+
+export async function gymsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', searchGymsController)
+  app.get('/gyms/nearby', listNearbyGymsController)
+
+  app.post('/gyms', createGymController)
+}
