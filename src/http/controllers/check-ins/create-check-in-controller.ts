1a58fd944ef58c6dc5cb3bff0335df1bca248792
Luiz Gustavo <gusstavopdm@gmail.com>
2023-04-12 08:30:17 -0300

    ✨ feat(create-check-in-controller.ts): add createCheckInController function to handle POST requests for creating check-ins. It receives a gymId, latitude and longitude in the request and creates a check-in for the authenticated user.

---
+++ src/http/controllers/check-ins/create-check-in-controller.ts
@@ -0,0 +1,36 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+
+import { makeCheckInUseCase } from '@/use-cases/check-ins/factories/make-check-in-usecase'
+
+export async function createCheckInController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const createCheckInParamsSchema = z.object({
+    gymId: z.string().uuid(),
+  })
+
+  const createCheckInBodySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { gymId } = createCheckInParamsSchema.parse(request.params)
+  const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
+
+  const createCheckInUseCase = makeCheckInUseCase()
+
+  await createCheckInUseCase.execute({
+    gymId,
+    userId: request.user.sub,
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(201).send()
+}
