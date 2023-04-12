293cb9376850225b5f7c273fd5638d0e2d3a21c2
Luiz Gustavo <gusstavopdm@gmail.com>
2023-04-12 08:49:23 -0300

    ✅ test(create-gym-controller.spec.ts): add e2e test for creating a gym with valid data

---
+++ src/http/controllers/gyms/create-gym-controller.spec.ts
@@ -0,0 +1,32 @@
+import request from 'supertest'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Create Gym (e2e)', () => {
+  beforeEach(async () => {
+    await app.ready()
+  })
+
+  afterEach(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: null,
+        phone: null,
+        latitude: -19.949112,
+        longitude: -44.178002,
+      })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
