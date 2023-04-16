617007e7852a65fb51a0736592bc190b9bee2611
Luiz Gustavo <gusstavopdm@gmail.com>
2023-04-16 10:50:11 -0300

    🚀 feat(routes.ts): add refresh token route to /token/refresh endpoint

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,10 +5,12 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { authenticateUserController } from './authenticate-user-controller'
 import { getUserProfileController } from './get-user-profile-controller'
 import { registerUserController } from './register-user-controller'
+import { refreshTokenController } from './refresh-token-controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', registerUserController)
   app.post('/sessions', authenticateUserController)
+  app.patch('/token/refresh', refreshTokenController)
 
   /* Authenticate */
   app.get('/me', { onRequest: [verifyJWT] }, getUserProfileController)
