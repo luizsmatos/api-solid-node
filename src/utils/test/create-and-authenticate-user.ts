edd685a05af46fb54a55d1d4ae8d6559eac19f53
Luiz Gustavo <gusstavopdm@gmail.com>
2023-04-16 11:17:11 -0300

    🚀 feat(create-and-authenticate-user.ts): add role parameter to createAndAuthenticateUser function

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,7 +1,22 @@
+import { Role } from '@prisma/client'
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify/types/instance'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  role: Role,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'Edgar Washington',
+      email: 'nedzesa@sip.cr',
+      password_hash: await hash('123456', 6),
+      role,
+    },
+  })
+
   await request(app.server).post('/users').send({
     name: 'Edgar Washington',
     email: 'nedzesa@sip.cr',
